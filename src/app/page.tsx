b8f8136f42b577962dc7db4216f2496dc77d3e64
Marylise <marylise@gmail.com>
2026-08-03 17:10:26 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,8 @@ export default function HomePage() {
           <p className="uppercase tracking-widest text-orange-200 font-semibold mb-2">
             We proudly donate 10% of our profit back to VT community! 
           </p>
-         
+          <p className="uppercase tracking-widest text-orange-200 font-semibold mb-2">
+            August's selected organization is: <a href="https://www.instagram.com/vt.enable/">e-NABLE</a></p>
           <h1 className="text-4xl sm:text-5xl font-extrabold max-w-xl leading-tight">
             Care packages made with LOVE
           </h1>
